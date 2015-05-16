9f497d82d645dd9151a96b21f85699e357eb06aa
FukenSytem <yaremchukvk@gmail.com>
2015-05-16 14:14:24 +0300

    with input
    with input changings

--- randomNumber.py
+++ randomNumber.py
@@ -2,13 +2,13 @@
 import random
 
 print "Lucky Numbers! 10 numbers will be generated."
-print "If one of them is a '5', you lose!"
-
+print "If one of them is a 'number that you input', you lose!"
+inputNumber = input("Input some number: ")
 count = 0
 while count < 10:
     num = random.randint(1, 11)
     print num
-    if num == 5:
+    if num == inputNumber:
         print "Sorry, you lose!"
         break
     count += 1
